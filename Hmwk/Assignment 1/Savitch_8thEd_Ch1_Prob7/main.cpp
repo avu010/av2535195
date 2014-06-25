dbf05efa768e08d6d9afc4fce7ff7ac4269a09df
avu010 <avu010@ucr.edu>
2014-06-25 16:52:30 -0700

    Signed-off-by: avu010 <avu010@ucr.edu>

---
+++ Hmwk/Assignment 1/Savitch_8thEd_Ch1_Prob7/main.cpp
@@ -0,0 +1,39 @@
+/* 
+ * File:   main.cpp
+ * Author: Anh Vu
+ * Purpose: Savitch Ch1 Prob7
+ * Created on June 25, 2014, 4:31 PM
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+
+//Output Border
+    cout<<"*******************************************************\n";
+    cout<<endl;
+    cout<<"            C C C          S S S S       !!\n";
+    cout<<"          C       C       S       S      !!\n";
+    cout<<"         C               S               !!\n";
+    cout<<"        C                 S              !!\n";
+    cout<<"        C                  S S S S       !!\n";
+    cout<<"        C                          S     !!\n";
+    cout<<"         C                          S    !!\n";
+    cout<<"          C       C      S         S       \n";
+    cout<<"            C C C          S S S S       00\n";
+    cout<<endl;
+    cout<<"*******************************************************\n";
+    cout<< endl;
+    cout<<"Computer Science is Cool Stuff!!!\n";
+    return 0;
+}
+
