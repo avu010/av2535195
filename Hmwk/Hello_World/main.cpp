406fe097759b648902735d7584663f54b433917f
avu010 <avu010@ucr.edu>
2014-06-24 19:49:08 -0700

    Signed-off-by: avu010 <avu010@ucr.edu>

---
+++ Hmwk/Hello_World/main.cpp
@@ -0,0 +1,24 @@
+/* 
+ * File:   main.cpp
+ * Author: Anh Vu  
+ * Purpose: First Hmwk
+ * Created on June 24, 2014, 7:32 PM
+ */
+
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes 
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+//Output Simple Text
+    cout<<"Hello World"<<endl;
+    //Exit Stage Right!
+    return 0;
+}
+
