85e0aee21aa4bc2cef0630aaeeba7c5d68db412e
avu010 <avu010@ucr.edu>
2014-06-25 16:25:30 -0700

    Signed-off-by: avu010 <avu010@ucr.edu>

--- Hmwk/Assignment 1/Savitch_8thEd_Ch1_Prob6/main.cpp
+++ Hmwk/Assignment 1/Savitch_8thEd_Ch1_Prob6/main.cpp
@@ -6,11 +6,11 @@
  */
 
 //System Level Libraries
-//Error: Extra Space between the < and iostream file name.
-//Error: Deleted > in include directive.
-//main.cpp:11:20: error: missing terminating > character
-//main.cpp:11:20: fatal error:  iostream: No such file or directory
-#include < iostream
+//Error: Extra space between < and iostream file name
+//main.cpp:10:21: fatal error:  iostream: No such file or directory
+//Error: Missing >
+//main.cpp:12:19: error: missing terminating > character
+#include <iostream>
 using namespace std;
 
 //User Libraries
@@ -20,28 +20,32 @@ using namespace std;
 //Function Prototypes
 
 //Execution Begins Here!
-//Error: Omit the int from int main()
-//Error: Omit the ()
-//Error: Mispelled 'main'
-     man int argc, char** argv {
+//Error: Omit int from int main()
+//no error message
+//Error: Omit ) from int main )
+//main.cpp:25:32: error: expected ')' before '{' token
+int main(int argc, char** argv) {
 //Variable Declaration
     short int1, int2, sum, prod;
 
 //Output Simple Text
-//Error: Mispelled cout
-    out<<"Please return after entering a number.\n";
+//Error: Misspelled cout
+//main.cpp:34:5: error: 'out' was not declared in this scope
+    cout<<"Please return after entering a number.\n";
     cout<<"Please enter first integer: ";
     
 //Input Data
-//Mispelled cin
-    in>>int1;
+//Error: Mispelled cin
+//main.cpp:39:5: error: 'in' was not declared in this scope
+    cin>>int1;
             
 //Output Simple Text
     cout<<"Please enter second integer: ";
     
-//Input Data 
+//Input Data
 //Error: Omit >
-    cin>int2;
+//main.cpp:48:8: error: no match for 'operator>' 
+    cin>>int2;
     
 //Calculations - sum and product of int1 and int2
     sum=int1+int2;
@@ -54,7 +58,4 @@ using namespace std;
     
 //Exit Stage Right!
     return 0;
-    
-//Error: Missing '}'
-
-
+}
