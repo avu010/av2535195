7d9552070ea2ce93986afd690511389be1a2e30d
avu010 <avu010@ucr.edu>
2014-07-02 11:34:41 -0700

    Signed-off-by: avu010 <avu010@ucr.edu>

--- Hmwk/Assignment2/Savitch_8thEd_Ch2_Prob15/main.cpp
+++ Hmwk/Assignment2/Savitch_8thEd_Ch2_Prob15/main.cpp
@@ -19,25 +19,31 @@ using namespace std;
 //Execution Begins Here!
 int main(int argc, char** argv) {
 //Declare Variables
+    //Input
     unsigned short sTemp;       //Starting Temperature
     unsigned short eTemp;       //Ending Temperature
+    //Output
     float vel;                  //Velocity of sound in dry air
     
-//Output Text and Input Data from user
-    cout<<"Please enter starting temperature in degrees Celsius. ";
-    cout<<"The starting temperature should be lower than the ending temperature.\n";
+//Output Text and Input Data 
+    cout<<"Be advised that this program will only work if starting temperature";
+    cout<<" is lower than ending temperature.\n";
+    cout<<endl;
+    cout<<"Please enter starting temperature in degrees Celsius: ";
     cin>>sTemp;
-    cout<<"Please enter ending temperature in degrees Celsius. \n";
+    cout<<"Please enter ending temperature in degrees Celsius: ";
     cin>>eTemp;
+    cout<<endl;
     
-//Use while loop to output velocities from starting temp to ending temp with increment of 1.
+//Calculate velocities from starting temp to ending temp with increment of 1.
     while (sTemp<=eTemp) {
         vel=331.3+0.61*sTemp;
         cout<<fixed<<showpoint<<setprecision(1);
         cout<<"At "<<sTemp<<" degrees Celsius the velocity of sound is "<<vel<<" m/s\n";
         sTemp=sTemp+1;
     }
-
+    
+//Exit Stage Right!
     return 0;
 }
 
