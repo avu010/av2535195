ca0e0e0d86063db50a48b0ff8626d8e3f3276d0f
avu010 <avu010@ucr.edu>
2014-07-01 17:24:48 -0700

    Signed-off-by: avu010 <avu010@ucr.edu>

--- Hmwk/Assignment2/Savitch_8thEd_Ch2_Prob1/nbproject/Makefile-impl.mk
+++ Hmwk/Assignment2/Savitch_8thEd_Ch2_Prob1/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=Savitch_8thEd_Ch2_Prob_edit
+PROJECTNAME=Savitch_8thEd_Ch2_Prob1
 
 # Active Configuration
 DEFAULTCONF=Debug
