9ebcb67dd80e83598206c1ba19243bee2e827b98
avu010 <avu010@ucr.edu>
2014-07-06 20:17:11 -0700

    Signed-off-by: avu010 <avu010@ucr.edu>

--- Hmwk/Assignment3/Savitch_8thEd_Ch3_Prob2/main.cpp
+++ Hmwk/Assignment3/Savitch_8thEd_Ch3_Prob2/main.cpp
@@ -22,32 +22,42 @@ int main(int argc, char** argv) {
     float bal;      //Account Balance
     float rate;     //Interest Rate
     float min;      //Minimum payment
+    char choice;    //Whether or not user wants to calculate again. (Y/N)
     //Output
     float totAmt;   //Total Amount Due
     
-//Gather Data Input
+//Calculate interest rate, total amount due, and minimum amount due.
+    do{        
+    //Gather Data Input
     cout<<"Enter your account balance: $";
     cin>>bal;
-    
-//Calculate interest rate, total amount due, and minimum amount due.
-    if (bal<=1000) {                //If balance is less than or equal to $1000
+    //If balance is less than or equal to $1000
+    if (bal<=1000) {              
         rate=1.5e-2;
-        totAmt=bal*rate+bal;    
-        if(totAmt<=10)      //Nested if else - if total amount is less than or equal to $10
-            min=10;                 
-        else                //Nested if else - If total amount is greater than $10
+        totAmt=bal*rate+bal; 
+        //Nested if - if total amount is less than or equal to $10
+        if(totAmt<=10)     
+            min=10;  
+        //Nested else - If total amount is greater than $10
+        else                
             min=totAmt;
     }
-    else {                          //If balance is greater than $1000
+    //If balance is greater than $1000
+    else {                         
         rate=1e-2;
         totAmt=(1.5e-2)*(1000)+rate*(bal-1000)+bal; 
         min=totAmt;
-    }
-
-//Output Results
+    }    
+    //Output Results
     cout<<fixed<<showpoint<<setprecision(2);
     cout<<"Total Amount Due: $"<<totAmt<<endl;
     cout<<"Minimum Payment: $"<<min<<endl;
+    cout<<endl;    
+    //Ask user if he or she wants to run the program again.
+    cout<<"Calculate again (Y/N)? ";
+    cin>>choice;
+    cout<<endl;
+    } while ((choice=='Y')||(choice=='y'));
     
     return 0;
 }
