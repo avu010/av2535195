12d61d9742ddd425b03969b4bfce5554440fe0be
avu010 <avu010@ucr.edu>
2014-07-06 20:27:55 -0700

    Signed-off-by: avu010 <avu010@ucr.edu>

--- Hmwk/Assignment3/Savitch_8thEd_Ch3_Prob1/main.cpp
+++ Hmwk/Assignment3/Savitch_8thEd_Ch3_Prob1/main.cpp
@@ -38,23 +38,23 @@ int main(int argc, char** argv) {
     cin>>plyr2;
     cout<< endl;
     
-    if (((plyr1='P')||(plyr1='p'))&&((plyr2='R')||(plyr2='r')))
-        cout<<"The winner is Paper! Paper covers rock.";
+    if (((plyr1=='P')||(plyr1=='p'))&&((plyr2=='R')||(plyr2=='r')))
+        cout<<"The winner is Player 1! Paper covers rock.";
   
-    else if (((plyr1='R')||(plyr1='r'))&&((plyr2='P')||(plyr2='p')))
-        cout<<"The winner is Paper! Paper covers rock.";
+    else if (((plyr1=='R')||(plyr1=='r'))&&((plyr2=='P')||(plyr2=='p')))
+        cout<<"The winner is Player 2! Paper covers rock.";
     
-    else if (((plyr1='S')||(plyr1='s'))&&((plyr2='P')||(plyr2='p')))
-        cout<<"The winner is Scissors! Scissors cut paper.";
+    else if (((plyr1=='S')||(plyr1=='s'))&&((plyr2=='P')||(plyr2=='p')))
+        cout<<"The winner is Player 1! Scissors cut paper.";
    
-    else if (((plyr1='P')||(plyr1='p'))&&((plyr2='S')||(plyr2='s')))
-        cout<<"The winner is Scissors! Scissors cut paper."; 
+    else if (((plyr1=='P')||(plyr1=='p'))&&((plyr2=='S')||(plyr2=='s')))
+        cout<<"The winner is Player 2! Scissors cut paper."; 
  
-    else if (((plyr1='R')||(plyr1='r'))&&((plyr2='S')||(plyr2='s'))) 
-        cout<<"The winner is Rock! Rock breaks scissors.";
+    else if (((plyr1=='R')||(plyr1=='r'))&&((plyr2=='S')||(plyr2=='s'))) 
+        cout<<"The winner is Player 1! Rock breaks scissors.";
    
-    else if (((plyr1='S')||(plyr1='s'))&&((plyr2='R')||(plyr2='r'))) 
-        cout<<"The winner is Rock! Rock breaks scissors.";
+    else if (((plyr1=='S')||(plyr1=='s'))&&((plyr2=='R')||(plyr2=='r'))) 
+        cout<<"The winner is Player 2! Rock breaks scissors.";
 
     else 
         cout<<"Nobody wins.";
