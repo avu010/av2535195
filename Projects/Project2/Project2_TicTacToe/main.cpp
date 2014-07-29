2b82a526f082593bf590292f1e9a973a6569b44f
avu010 <avu010@ucr.edu>
2014-07-29 15:07:07 -0700

    Signed-off-by: avu010 <avu010@ucr.edu>

--- Projects/Project2/Project2_TicTacToe/main.cpp
+++ Projects/Project2/Project2_TicTacToe/main.cpp
@@ -15,19 +15,19 @@ using namespace std;
 //User Libraries
 
 //Global Constants
-const int COL=3;
+const int COL=3;        //Size of columns of grid
 
 //Function Prototypes
 void welcome();
-void displayGrid(char [][COL], const int);    //Displays Tic-Tac-Toe board
-void takeTurn(char [][COL], const int , bool);//Places appropriate mark on board 
-bool gameOver(char[][COL], const int);  //Determines whether or not game is over
-short winner(char[][COL],const int);          //Determines winner
-void menu();                                  //Displays menu
-void reset (char [][COL], const int);         //Resets board 
-void file (int, const int);                   //Outputs scores to file
-void sort(char[][COL], const int);            //Sorts board
-void swap(char&, char&);                      //Swap values used for sort
+void displayGrid(char[][COL],const int);   //Displays Tic-Tac-Toe board
+void takeTurn(char[][COL],const int,bool); //Places appropriate mark on board 
+bool gameOver(char[][COL],const int);  //Determines whether or not game is over
+short winner(char[][COL],const int);       //Determines winner
+void menu();                               //Displays menu
+void reset (char[][COL],const int);        //Resets board 
+void file (int,const int);                 //Outputs scores to file
+void sort(char[][COL],const int);          //Sorts board
+void swap(char&,char&);                    //Swap values used for sort
 void stall();                 //Stalls program so it can print out sorts slower                                            
 void conversion(char,int&,int&);        //Converts user's choice to coordinates
 
@@ -35,44 +35,44 @@ void conversion(char,int&,int&);        //Converts user's choice to coordinates
 //Execution Begins Here:
 int main(int argc, char** argv) {
     //Declare and Initialize Variables
-    const int ROW=3;
-    //Array used for board
-    //I included an extra row and column so I could locate the variables
-    //easier
-    char grid[ROW][COL];  
+    const int ROW=3;                //Size of rows
+    char grid[ROW][COL];            //Array used to print grid    
     bool p1;                        //Used to determine who's turn it is
     short win;                      //Used to determining winner
     unsigned short score1=0;        //Calculate score for player 1
     unsigned short score2=0;        //Calculate score for player 2
     char again;                     //Whether players want to play again
     bool over;                      //Determines if game is over  
-    bool first;                     //Determines who's turn it is
+    bool first;                     //Used to switch between players
     
     //Display Menu
     menu();
     
     //Welcome Message
-    welcome ();
+    welcome();
     
    //Initialize board
-    //Open and input data from file
-    ifstream board;
-    board.open("board.txt");
+   //Open and input data from file
+   ifstream board;
+   board.open("board.txt");
    
-    //Read Data into array
-    for (int i=0; i<ROW; i++){
-        for (int j=0; j<COL; j++)
-            board>>grid[i][j];
+   //Read Data into array
+   for (int i=0; i<ROW; i++){
+       for (int j=0; j<COL; j++)
+           board>>grid[i][j];
     }
+    
     //Close file
     board.close();
     
     //Reiterates game until players decide to stop
     do{
-        //Game continues until game is over
+        //Game continues until it is over
         do{
-            displayGrid(grid, ROW);          //Display Grid
-            takeTurn(grid,ROW, first);       //User takes turn
+            //Display Grid
+            displayGrid(grid,ROW);   
+            //User takes turn
+            takeTurn(grid,ROW,first);      
             //Switches between players 1 & 2
             if(first){                  
                 first=false;
@@ -95,7 +95,10 @@ int main(int argc, char** argv) {
         //Displays winner
         win = winner(grid,ROW);
 
-        //Keep track of scores 
+        //Keep track of scores
+        //win=1, player 1 wins
+        //win=0, player 2 wins
+        //win=-1; no one wins
         if (win==1){
             score1++;
         }else if (win==0){
@@ -112,6 +115,7 @@ int main(int argc, char** argv) {
         cout<<"Play again (Y/N)? ";
         cin>>again;
         
+        //Sorts grid and prints out dance before board resets
         sort(grid,ROW);
         
         //Reset board so user can play again
@@ -119,28 +123,30 @@ int main(int argc, char** argv) {
     }while((again=='Y')||(again=='y'));
  
     //Exit Stage Right!
-    return 0;
-    
+    return 0;   
 }
 
 //Function displays grid
 void displayGrid(char grid[][COL], const int ROW){
     //Board with variables at designated positions
     cout<<"             |          |          "<<endl;
-    cout<<"       "<<grid[0][0]<<"     |    "<<grid[0][1]<<"     |    "<<grid[0][2]<<endl;
+    cout<<"       "<<grid[0][0]<<"     |    "<<grid[0][1]<<"     |    ";
+    cout<<grid[0][2]<<endl;
     cout<<"   __________|__________|__________"<<endl;
     cout<<"             |          |          "<<endl;
-    cout<<"       "<<grid[1][0]<<"     |    "<<grid[1][1]<<"     |    "<<grid[1][2]<<endl;
+    cout<<"       "<<grid[1][0]<<"     |    "<<grid[1][1]<<"     |    ";
+    cout<<grid[1][2]<<endl;
     cout<<"   __________|__________|__________"<<endl;
     cout<<"             |          |          "<<endl;
-    cout<<"       "<<grid[2][0]<<"     |    "<<grid[2][1]<<"     |    "<<grid[2][2]<<endl;
+    cout<<"       "<<grid[2][0]<<"     |    "<<grid[2][1]<<"     |    ";
+    cout<<grid[2][2]<<endl;
     cout<<"             |          |          "<<endl;
 }
 
 //Displays welcome message as well as randomly chooses who plays first
 void welcome(){
     //Welcome player and output which player is what mark
-    cout<<"Welcome to Tic-Tac-Toe! Who will play first will be randomly chosen.";
+    cout<<"Welcome to Tic-Tac-Toe! First player will be randomly chosen.";
     cout<<" May the best man win!"<<endl;
     cout<<"Player 1= X"<<endl;
     cout<<"Player 2= O"<<endl<<endl;
@@ -161,15 +167,15 @@ void welcome(){
     }
 }
 
-//Function places mark on board
+//Places mark on board
 void takeTurn(char grid[][COL], const int ROW, bool p1){
     //Declare Variables
-    char ch;
-    int row;
-    int column;
+    char ch;            //User's choice
+    int row;            //Row coordinate
+    int column;         //Column coordinate
     
     //Gather Data Input
-    //Takes user's choices as long as spaces are available
+    //Takes user's choices as long as it's valid
     do{
         if(p1){
             cout<<"PLAYER 1'S TURN"<<endl;
@@ -178,14 +184,14 @@ void takeTurn(char grid[][COL], const int ROW, bool p1){
         }
         cout<<"Enter number: ";
         cin>>ch;
+        //Converts user's choice to coordinate on game board
         conversion(ch,row,column);
     }while(row==3||column==3);
         
     //If player one makes mark, place an X. If player 2, place O.
         if(p1){
         grid[row][column] = 'X';
-        }
-        else{
+        }else{
         grid[row][column] = 'O';
         }
 }
@@ -193,10 +199,9 @@ void takeTurn(char grid[][COL], const int ROW, bool p1){
 //Determines whether game is over
 bool gameOver(char grid[][COL], const int ROW){
     //Declare Variables
-    bool over=false;        
+    bool over=false;                 
     
-    //Determine if game is over
-    //Game is over if one of the players wins, or if all the spaces are taken up
+    //Game over conditions
     //Checks for rows
     for(int i=0; i<ROW; i++){
         if((grid[i][0]==grid[i][1])&&(grid[i][0]==grid[i][2]))
@@ -253,8 +258,8 @@ short winner(char grid[][COL], const int ROW){
         }
     }
     
-    //Used to find wins in diagonals. Will only execute if no one wins 
-    //with columns and rows
+    //Used to find wins in diagonals. Will only execute if statements above
+    //aren't true
     if(!(win==1||win==0)){
         if ((grid[0][0]=='X')&&(grid[1][1]=='X')&&(grid[2][2]=='X')){
                 cout<<"Player 1 wins!"<<endl;
@@ -283,7 +288,8 @@ short winner(char grid[][COL], const int ROW){
 //Menu 
 void menu(){
     //Declare Variables
-    char choice;
+    char choice;                    
+    
     //Prompt user to input choice
     do{
         cout<<"Main Menu:  "<<endl;
@@ -300,17 +306,18 @@ void menu(){
         case '2':
             cout<<"Rules:  "<<endl;
             cout<<"1. Decide who will be Player 1 or Player 1"<<endl;
-            cout<<"2. When making your move, choose the location ";
-            cout<<" to place your mark by choosing the column and row."<<endl;
-            cout<<"3. You may only enter 1, 2, or 3 for both columns and ";
-            cout<<"rows. If you attempt to choose an invalid column or row, \n";
-            cout<<"   you will be asked to make another move."<<endl;
-            cout<<"4. Try to get all three of your marks in a row, ";
+            cout<<"2. When making your move, choose a number (1-9) to place ";
+            cout<<"your mark. "<<endl;
+            cout<<"4. If you attempt to choose a number lower than 1 or ";
+            cout<<"higher than 9 or if the space is already taken, you will";
+            cout<<" be asked to choose another spot."<<endl;
+            cout<<"5. Try to get all three of your marks in a row, ";
             cout<<" column, or diagonal row before the other player does.\n";
-            cout<<"5. After each game, decide if you want to play again.\n";
-            cout<<"6. If you want to see a tally of your scores, see the ";
+            cout<<"6. After each game, choose 'Y' if you want to play again";
+            cout<<" or 'N' if you don't. "<<endl;
+            cout<<"7. If you want to see a tally of your scores, see the ";
             cout<<"text file entitled scores."<<endl;
-            cout<<"7. Have fun!"<<endl<<endl;
+            cout<<"8. Have fun!"<<endl<<endl;
             break;  
     }   
 }
@@ -334,6 +341,7 @@ void file (int score1, int score2){
     output<<"Player 1: "<<score1<<" wins & "
           <<"Player 2: "<<score2<<" wins...........";
 
+    //Compares scores between player 1 and player 2
     if(score1<score2)
         output<<"Player 1, you need to step up your game!\n";       
     else if (score2<score1)
@@ -348,32 +356,37 @@ void file (int score1, int score2){
 
 //Sorts grid out after game is over
 void sort(char grid[][COL], const int ROW){
-    const int TOTAL = 9;
+    //Declare Variables
+    const int TOTAL = 9;        //Number of elements in the board
+    
+    //Loops through enter array to bubble sort elements
     for(int i=0; i<TOTAL; i++){
         for(int j=i+1;j<TOTAL;j++){
             if(grid[0][i]>grid[0][j]){
+                //Swap function
                 swap(grid[0][i],grid[0][j]);
+                //Displays new grid after each sort
                 displayGrid(grid,ROW);
+                //Stalls program in between each sort
                 stall();
                 cout<<endl;
             }
-
-            }
         }
     }
+}
      
-
 //Used to swap values in bubble sort
 void swap(char& x, char& y){
-    char temp;
+    //Declare Variables
+    char temp;          //Temporary Variable
     temp=x;
     x=y;
     y=temp;
 }
 
-//Stalls printing of the graph sorting itself out
+//Stalls printing of the graph each time it sorts itself
 void stall(){
-    for(int i=0;i<140000000;i++);
+    for(int i=0;i<100000000;i++);
 }
 
 //Converts user's choice to coordinates on grid
@@ -388,8 +401,7 @@ void conversion(char ch, int &r,int &c){
         case '7': r=2;c=0;break;
         case '8': r=2;c=1;break;
         case '9': r=2;c=2;break;
-        default:
-            r=3;c=3;
+        default: r=3;c=3;
     }
 }    
 
